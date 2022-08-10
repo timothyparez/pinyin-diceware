45a72dd033e1e68ecde211615d6dd6ad84e2085f
Timothy Parez <timothyparez@outlook.com>
2022-08-10 13:19:16 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using static System.Console;
+
+const int LENGTH = 5;
+const int COUNT = 10;
+const int INDEX_DIGITS = 5;
+
+using var rnd = RandomNumberGenerator.Create();
+
+var wordList = File.ReadAllLines("pinyin.wordlist")
+                   .Select(l => l.Split('\t', StringSplitOptions.RemoveEmptyEntries))
+                   .Select(i => (int.Parse(i[0]), i[1], i[2]))
+                   .ToDictionary(i => i.Item1);
+
+var buffer = new byte[1];
+
+for (int c = 0; c < COUNT; c++)
+{
+    var pinyinPhraseBuilder = new StringBuilder();
+    var phraseBuilder = new StringBuilder();
+
+    for (int i = 0; i < LENGTH; i++)
+    {
+        var wordIndexBuilder = new StringBuilder(5);
+
+        for (int y = 0; y < INDEX_DIGITS; y++)
+        {
+            rnd.GetBytes(buffer);
+            wordIndexBuilder.Append(buffer[0] % 6 + 1);
+        }
+
+        var wordIndex = int.Parse(wordIndexBuilder.ToString());
+        var word = wordList[wordIndex];
+
+        phraseBuilder.Append($"{word.Item3} ");
+        pinyinPhraseBuilder.Append($"{word.Item2} ");
+    }
+
+    WriteLine($"{pinyinPhraseBuilder} ({phraseBuilder})");
+}
---
+++ readme.md
@@ -0,0 +1,28 @@
+# Pinyin-Diceware
+
+This is a simple diceware password generator inspired by [dmuth/diceware](https://diceware.dmuth.org/).   
+
+Instead of the default `English` wordlist, it uses the `Chinese/Pinyin` wordlist from [chinese-diceware](https://github.com/cfbao/chinese-diceware).
+
+(The only reason this repository is GPL licensed is because those two projects are)
+
+
+## Usage 
+
+1. Install (.NET6)[https://dotnet.microsoft.com/en-us/download/dotnet/6.0]
+2. Execute `dotnet run`
+
+## Example output
+
+```
+eluosi caigan yixing baochi yangji  (俄罗斯 才干 一行 保持 殃及 )
+heai eer daoxie fenwen benpao  (和蔼 俄而 道谢 分文 奔跑 )
+juece guohao waisun toumo pinxue  (决策 国号 外孙 偷摸 贫血 )
+jiliu genzi miejue youhei diren  (激流 根子 灭绝 黝黑 敌人 )
+weiyan niqiu shunv yinjiu kafei  (威严 泥鳅 淑女 饮酒 咖啡 )
+sheruo qingre sihuo zuojia longya  (设若 清热 死活 作家 聋哑 )
+zhihui suoyao tongji milan motuo  (指挥 索要 统计 糜烂 摩托 )
+meise munian fawen weiqie biaoge  (美色 暮年 发问 畏怯 表格 )
+xiayue mingri daoban fushui mingci  (下月 明日 盗版 赋税 名词 )
+zhangu tuoci zhusun xuebai qiquan  (战鼓 托辞 竹笋 雪白 齐全 )
+```
